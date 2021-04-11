620b6520a3c2a984699bc4d153245b881c400375
Eduardo Cristiano Thums <eduardocristiano01@gmail.com>
2021-04-11 00:43:03 -0300

    Refactor script that generate chart

--- Makefile
+++ Makefile
@@ -1,28 +1,26 @@
-.PHONY: calculate-code-complexity calculate-churn freeze-dependencies identify-hotspots
-.SILENT: calculate-code-complexity calculate-churn identify-hotspots
+.PHONY: run-radon-analysis calculate-churn freeze-dependencies identify-hotspots
+.SILENT: run-radon-analysis calculate-churn identify-hotspots
 
-calculate-code-complexity:
-	radon cc -na -i venv -e "*_test.py" -j -O radon-cc-output.json .
-	python ./calculate_code_complexity.py
-	rm radon-cc-output.json
+run-radon-analysis:
+	radon cc -na -i venv -i .env -i migrations -e "*_test.py" -j -O radon-cc-output.json $(path)
 
 calculate-churn:
-	git log --format=format: --name-only --since=12.month \
-	| egrep -v "^$" \
-	| egrep ".py$" \
-	| egrep -v "*_test.py$" \
-	| egrep -v "^__init__.py$" \
+	git --git-dir $(path)/.git log --format=format: --name-only --since=12.month \
+	| egrep -v "^$$" \
+	| egrep ".py$$" \
+	| egrep -v "*_test.py$$" \
+	| egrep -v "^__init__.py$$" \
 	| sort \
 	| uniq -c \
 	| sort -nr \
 	| head -50 \
-	| grep -E '[a-zA-Z_\/]*.py' -o \
 	> churn.list
 
 identify-hotspots:
-	make calculate-code-complexity
-	make calculate-churn
+	make run-radon-analysis path=$(path)
+	make calculate-churn path=$(path)
 	python ./generate_graph.py
+
 
 freeze-dependencies:
 	pip freeze > requirements.txt
--- generate_graph.py
+++ generate_graph.py
@@ -1,27 +1,66 @@
+import json
+
 import matplotlib.pyplot as plt
 
+RADON_CC_OUTPUT_PATH = './radon-cc-output.json'
+CHURN_OUTPUT_PATH = './churn.list'
+
+
+def calculate_code_complexity():
+    code_complexity = {}
+
+    with open(RADON_CC_OUTPUT_PATH) as file:
+        radon_analysis = json.load(file)
+
+        for file_name, blocks in list(radon_analysis.items())[:50]:
+            total_complexity = sum([block['complexity'] for block in blocks])
+            average_complexity = round((total_complexity / len(blocks)), 2)
+
+            code_complexity[file_name] = average_complexity
+
+        return code_complexity
+
+
+def calculate_churn():
+    churn = {}
+
+    with open(CHURN_OUTPUT_PATH) as file:
+        for line in file.readlines():
+            churn_per_file = line.strip().replace('\n', '').split(' ')
+
+            changed_times = churn_per_file[0]
+            file_name = churn_per_file[1]
+
+            churn[file_name] = changed_times
+
+    return churn
+
+
 if __name__ == '__main__':
-    with open('./code-complexity.list') as cc_file, open('./churn.list') as churn_file:
-        cc_files = [line.replace('\n', '') for line in cc_file.readlines()]
-        churn_files = [line.replace('\n', '') for line in churn_file.readlines()]
+    churn = calculate_churn()
+    code_complexity = calculate_code_complexity()
 
-        cc_length = len(cc_files)
-        churn_length = len(churn_files)
+    churn_values = list(churn.values())[::-1]
+    code_complexity_values = list(code_complexity.values())[::-1]
 
-        if cc_length > churn_length:
-            cc_files = cc_files[:churn_length]
+    churn_length = len(churn_values)
+    code_complexity_length = len(code_complexity_values)
 
-        elif churn_length > cc_length:
-            churn_files = churn_files[:cc_length]
+    if code_complexity_length > churn_length:
+        code_complexity_values = code_complexity_values[:churn_length]
 
-        plt.xlabel('Churn')
-        plt.ylabel('Code Complexity')
+    elif churn_length > code_complexity_length:
+        churn_values = churn_values[:code_complexity_length]
 
-        plt.scatter(
-            x=cc_files,
-            y=churn_files
-        )
+    plt.xlabel('Churn')
+    plt.ylabel('Code Complexity')
 
-        plt.show()
+    plt.scatter(
+        x=churn_values,
+        y=code_complexity_values
+    )
 
-        plt.savefig('./code-complexity-vs-churn.jpeg')
+    # TODO: find a better way to store the generated chart with reasonable dimensions
+    # plt.savefig('./code-complexity-vs-churn.jpeg')
+
+    plt.show()
